b4022ff1ff37dd4f2192c0c887861743219f770b
toguri <oguri_toru@cyberagent.co.jp>
2015-03-18 16:06:48 +0900

    試してみた

---
+++ LeakTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeakTest
+//
+//  Created by A13694 on 2015/03/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeakTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeakTest
+//
+//  Created by A13694 on 2015/03/18.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
